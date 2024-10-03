9afcf479a88b57f7cc197c04d19114a562447e9e
FoxyHopper Dev <117083790+DemXc@users.noreply.github.com>
2024-10-03 10:46:44 +0600

    update main.py

--- main.py
+++ main.py
@@ -5,9 +5,11 @@ from PIL import Image
 import torch 
 from transformers import GLPNImageProcessor, GLPNForDepthEstimation
 
+# Загрузка процессора изображения и модели
 attribute_selector = GLPNImageProcessor.from_pretrained("vinvino02/glpn-nyu")
 model = GLPNForDepthEstimation.from_pretrained("vinvino02/glpn-nyu")
 
+# Открытие изображения
 image = Image.open("X-RAY.jpg")
 new_height = 480 if image.height > 480 else image.height
 new_height -= (new_height % 32)
@@ -18,21 +20,28 @@ new_width = new_width - diff if diff < 16 else new_width + 32 - diff
 new_size = (new_width, new_height)
 image = image.resize(new_size)
 
+# Подготовка изображения для модели
 inputs = attribute_selector(images=image, return_tensors="pt")
 
+# Прогнозирование глубины без отслеживания градиентов
 with torch.no_grad():
     outputs = model(**inputs)
     predicted_depth = outputs.predicted_depth
 
+# Обработка результатов
 pad = 16
 output = predicted_depth.squeeze().cpu().numpy() * 1000.0
 output = output[pad: -pad, pad: -pad]
 image = image.crop((pad, pad, image.width - pad, image.height - pad))
 
+# Отображение результатов
 fig, ax = plt.subplots(1, 2)
 ax[0].imshow(image)
 ax[0].tick_params(left=False, bottom=False, labelleft=False, labelbottom=False)
 ax[1].imshow(output, cmap='plasma')
 ax[1].tick_params(left=False, bottom=False, labelleft=False, labelbottom=False)
 plt.tight_layout()
+
+# Пауза на 5 секунд и отображение графиков
 plt.pause(5)
+plt.show()  # Ожидание закрытия окна пользователем
